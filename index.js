cb81bf01b2e7540ad10bb49f7b4d6be9a4462eee
Shyrz <i@shyrz.ch>
2021-08-07 21:21:29 +0800

    chore: add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import './HanYi/HYXuanSong/HYXuanSong35S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong45S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong55S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong65S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong75S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong85S.woff2';
+import './HanYi/HYXuanSong/HYXuanSong95S.woff2';
+
+import './HanYi/HYJunHei/HYJunHei-45W.woff2';
+import './HanYi/HYJunHei/HYJunHei-55W.woff2';
+import './HanYi/HYJunHei/HYJunHei-75W.woff2';
+import './HanYi/HYJunHei/HYJunHei-85W.woff2';
